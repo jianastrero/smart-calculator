b0270dc6c842c7afdcb9a08ebd1b1aa6a7725e7c
Jian James P. Astrero <jiana@goetu.com>
2020-05-02 16:00:00 +0800

    Update to postfix - will optimize

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -2,6 +2,8 @@ package calculator
 
 import java.lang.Exception
 import java.util.Scanner
+import java.util.Stack
+import kotlin.math.pow
 
 fun main() {
 
@@ -33,13 +35,15 @@ fun main() {
                     println(smartCalculator.evaluate(input))
                 }
             }
-        }catch (e: Exception) {
+        } catch (e: Exception) {
+            e.printStackTrace()
             println(e.message)
         }
     }
 }
 
-// 3 8 4 3 + 2 * 1 + * + 6 2 1 + / -
+// 8 * 3 + 12 * (4 - 2)
+
 class SmartCalculator {
     private val variables = mutableMapOf<String, String>()
 
@@ -66,15 +70,127 @@ class SmartCalculator {
         }
     }
 
+    private fun postfix(equation: String): MutableList<String> {
+        return try {
+            val equation = equation.split("\\s+".toRegex())
+
+            val list = mutableListOf<String>()
+            val operatorStack = Stack<Char>()
+
+            if (equation.size == 1 && equation[0].isValidValue()) {
+                list.add(equation[0])
+            } else {
+                var index = 0
+                while (equation.size > index) {
+                    val str = equation[index]
+
+                    if (str[0].isOperator()) {
+                        val lastOperator: Char? = operatorStack.lastOrNull()
+                        if (lastOperator.getPriority() >= str[0].getPriority()) {
+                            list.add("${operatorStack.pop()}")
+                        }
+                        operatorStack.push(str[0])
+                    } else if (str[0] == '(') {
+                        var nextEquation = str
+                        index++
+
+                        while (equation.size > index) {
+                            nextEquation += equation[index]
+
+                            if (nextEquation.count { it == '(' } == nextEquation.count { it == ')' }) {
+                                val nextStack = postfix(fixEquation(nextEquation.substring(1, nextEquation.length - 1)))
+                                list.addAll(nextStack)
+                                if (operatorStack.isNotEmpty()) {
+                                    list.add(operatorStack.pop().toString())
+                                }
+                                break
+                            }
+
+                            index++
+                        }
+                    } else {
+                        list.add(str)
+                    }
+
+                    index++
+                }
+
+                while (operatorStack.isNotEmpty()) {
+                    list.add(operatorStack.pop().toString())
+                }
+            }
+
+            list
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw  InvalidExpressionException()
+        }
+    }
+
     fun evaluate(equation: String): Int {
+        try {
+            val fixedEquation = fixEquation(equation)
+            val postfix = postfix(fixedEquation)
+            val stack = Stack<Int>()
+
+            while (postfix.isNotEmpty()) {
+
+                val x = postfix.first()
+                postfix.removeAt(0)
+
+                if (x.isValidValue()) {
+                    stack.push(x.toInt())
+                } else {
+                    val b = stack.pop()
+                    val a = stack.pop()
+
+                    val y = when (x) {
+                        "^" -> {
+                            a.toFloat().pow(b).toInt()
+                        }
+                        "*" -> {
+                            a * b
+                        }
+                        "/" -> {
+                            a / b
+                        }
+                        "+" -> {
+                            a + b
+                        }
+                        "-" -> {
+                            a - b
+                        }
+                        else -> {
+                            0
+                        }
+                    }
+
+                    stack.push(y)
+                }
+            }
+
+            return stack.pop()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw InvalidExpressionException()
+        }
+    }
+
+    private fun fixEquation(equation: String): String {
         val operators = equation.split("\\s*[^+\\-/*]+\\s*".toRegex())
             .filter { it.isNotEmpty() }
         val operands = equation.split("\\s*[+\\-/*]+\\s*".toRegex())
             .mapIndexed { index, s ->
-                if (index == 0 && s.isEmpty()) {
+                val t = s.replace("\\s+", "")
+                if (index == 0 && t.isEmpty()) {
                     0
-                } else if (s.isNotEmpty()) {
-                    getValue(s).toInt()
+                } else if (t.isNotEmpty()) {
+
+                    val x = t.replace("[()]+".toRegex(), "")
+                    val prefix = t.replace("[\\da-zA-Z]+[()]*".toRegex(), "")
+                    val suffix = t.replace("[()]*[\\da-zA-Z]+".toRegex(), "")
+
+                    "$prefix${getValue(x).toInt()}$suffix"
                 } else {
                     throw InvalidExpressionException()
                 }
@@ -84,25 +200,34 @@ class SmartCalculator {
         if (operators.size != operands.size - 1)
             throw InvalidExpressionException()
 
-        operators.forEachIndexed { index, s ->
-            when (simplifyOperator(s)) {
-                "+" -> operands[index + 1] = operands[index] + operands[index + 1]
-                "-" -> operands[index + 1] = operands[index] - operands[index + 1]
-                "*" -> operands[index + 1] = operands[index] * operands[index + 1]
-                "/" -> operands[index + 1] = operands[index] / operands[index + 1]
-            }
+        var x = "${operands[0]}"
+
+        operators.indices.forEach {
+            x += " ${simplifyOperator(operators[it])} ${operands[it + 1]}"
         }
 
-        return operands[operands.size - 1]
+        return x
+    }
+
+    private fun Char?.getPriority(): Int {
+        this?.let { operator ->
+            arrayOf("+-".toCharArray(), "*/".toCharArray(), charArrayOf('^'))
+                .forEachIndexed { index, it ->
+                    if (operator in it)
+                        return index
+                }
+        }
+
+        return -1
     }
 
     private fun getValue(variable: String): String {
         var value = variables[variable]
 
-        if (value != null && !value.isValidValue()) {
-            value = getValue(value)
-        } else if (variable.isValidValue()) {
+        if (variable.isValidValue()) {
             return variable
+        } else if (value != null && !value.isValidValue()) {
+            value = getValue(value)
         }
 
         return value ?: throw UnknownVariableException()
@@ -129,6 +254,8 @@ class SmartCalculator {
     private fun String?.isValidVariableName() = this?.matches("[A-Za-z]+".toRegex()) ?: false
 
     private fun String?.isValidValue() = this?.matches("\\d+".toRegex()) ?: false
+
+    private fun Char?.isOperator() = this?.let { it in "^*/+-".toCharArray() } ?: false
 
     class InvalidExpressionException : Exception("Invalid expression")
     class InvalidIdentifierException : Exception("Invalid identifier")
